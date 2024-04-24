5b1d7cf9970df621ffb513fc97d169dd587a926c
izzhamburhan <maizzham@gmail.com>
2024-04-24 11:36:29 +0800

    my-account-page

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import store from '../store'
+
 import HomeView from '../views/HomeView.vue'
 import ProductView from '../views/ProductView.vue'
 import CategoryView from '../views/CategoryView.vue'
@@ -6,6 +9,7 @@ import SearchView from '../views/SearchView.vue'
 import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
+import MyAccount from '../views/MyAccount.vue'
 
 const routes = [
   {
@@ -48,11 +52,27 @@ const routes = [
     name: 'LogIn',
     component: LogIn
   },
+  {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'Login', query: {to: to.path } } );
+  } else {
+    next()
+  }
 })
 
 export default router
